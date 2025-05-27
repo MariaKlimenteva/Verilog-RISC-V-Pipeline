0b0779f7cbf3e213e3ae52778ab67e7a32b7464d
Maria <klimentieva.mr@phystech.edu>
2025-05-27 08:45:33 +0300

    Add store byte/ load byte
    Signed-off-by: Maria <klimentieva.mr@phystech.edu>

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -4,8 +4,8 @@
 `timescale 1ns / 1ps
 module control_unit #() (
     input wire [6:0] opcode,
-    input wire [2:0] funct3,
-    input wire [6:0] funct7,
+    input wire [2:0] func3,
+    input wire [6:0] func7,
     output control_signals control_out_s
 );
     always_comb begin
@@ -13,13 +13,13 @@ module control_unit #() (
         case (opcode)
             `OPCODE_REG: begin // R-type: ADD/SUB/AND/OR/SLL/SRL/SRA/SLT/SLTU
                 control_out_s.RegWrite = 1'b1;
-                case (funct3)
-                    3'b000: control_out_s.ALUOp = (funct7[5]) ? `ALU_OP_SUB : `ALU_OP_ADD;
+                case (func3)
+                    3'b000: control_out_s.ALUOp = (func7[5]) ? `ALU_OP_SUB : `ALU_OP_ADD;
                     3'b001: control_out_s.ALUOp = `ALU_OP_SLL;
                     3'b010: control_out_s.ALUOp = `ALU_OP_SLT;
                     3'b011: control_out_s.ALUOp = `ALU_OP_SLTU;
                     3'b100: control_out_s.ALUOp = `ALU_OP_XOR;
-                    3'b101: control_out_s.ALUOp = (funct7[5]) ? `ALU_OP_SRA : `ALU_OP_SRL;
+                    3'b101: control_out_s.ALUOp = (func7[5]) ? `ALU_OP_SRA : `ALU_OP_SRL;
                     3'b110: control_out_s.ALUOp = `ALU_OP_OR;
                     3'b111: control_out_s.ALUOp = `ALU_OP_AND;
                     default: control_out_s.ALUOp = 4'bxxxx;
@@ -29,14 +29,14 @@ module control_unit #() (
             `OPCODE_IMM: begin // I-type
                 control_out_s.RegWrite = 1'b1;
                 control_out_s.ALUSrc = 1'b1; 
-                case (funct3)
+                case (func3)
                     3'b000: control_out_s.ALUOp = `ALU_OP_ADD;  // ADDI
                     3'b001: control_out_s.ALUOp = `ALU_OP_SLL;  // SLLI
                     3'b010: control_out_s.ALUOp = `ALU_OP_SLT;  // SLTI
                     3'b011: control_out_s.ALUOp = `ALU_OP_SLTU; // SLTIU
                     3'b100: control_out_s.ALUOp = `ALU_OP_XOR;  // XORI
                     3'b101: begin
-                        if (funct7[5])
+                        if (func7[5])
                             control_out_s.ALUOp = `ALU_OP_SRA; //SRAI
                         else
                             control_out_s.ALUOp = `ALU_OP_SRL; // SRLI
@@ -51,19 +51,21 @@ module control_unit #() (
                 control_out_s.RegWrite = 1'b1;
                 control_out_s.MemToReg = 1'b1; 
                 control_out_s.MemRead = 1'b1; 
+                control_out_s.LoadType = load_type_t'(func3);
                 control_out_s.ALUSrc  = 1'b1;
                 control_out_s.ALUOp   = `ALU_OP_ADD; 
             end
 
             `OPCODE_STORE: begin // S-type: SW/SH/SB
                 control_out_s.MemWrite = 1'b1; 
+                control_out_s.StoreType = store_type_t'(func3);
                 control_out_s.ALUSrc = 1'b1; 
-                control_out_s.ALUOp = `ALU_OP_ADD; 
+                control_out_s.ALUOp = `ALU_OP_ADD;
             end
 
             `OPCODE_BRANCH: begin // B-type: BEQ/BNE/BLT/BGE/BLTU/BGEU
                 control_out_s.Branch = 1'b1; 
-                case (funct3)
+                case (func3)
                     3'b000: control_out_s.BranchType = 3'b000; // BEQ
                     3'b001: control_out_s.BranchType = 3'b001; // BNE
                     3'b100: control_out_s.BranchType = 3'b100; // BLT
